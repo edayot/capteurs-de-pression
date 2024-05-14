5064fb2be533ad7daae8d885c87c42f283149aa6
edayot <pro.e.dayot@gmail.com>
2024-05-14 15:39:56 +0200

    state machine

--- capteurs-de-pression.ino
+++ capteurs-de-pression.ino
@@ -20,32 +20,80 @@ void setup()
     Serial.println("Hello World");
 
     servo.attach(MOTOR);
+    servo.write(0);
 }
 
+
+int state = 0;
+int attente_max = 100;
+int attente = 0;
+
+int tps_rempli = 0;
+// 0 == standby
+// 1 == verre détecté
+// 2 == remplissage
+// 3 == verre détecté plein, on attend qu'il soit retiré
 
 void loop()
 {   
     int value0 = analogRead(IN0);
     int value1 = analogRead(IN1);
-    int value2 = analogRead(IN2);
+    int value2 = analogRead(IN2);  
 
     int sum = value0 + value1 + value2;
 
     
-    Serial.print(String(value0) + " " + String(value1) + " " + String(value2) + " " + String(sum) + "\n");
+    Serial.print(String(sum) + " " + String(state) + " " + String(attente) + " " + String(attente_max) + "\n");
 
 
-    if (sum < 300)
+    // match case
+    switch (state)
     {
-        servo.write(0);
-    }
-    else if (sum >= 300 && sum < 1200)
-    {
-        servo.write(180);
-    }
-    else if (sum >= 1200)
-    {
-        servo.write(0);
+        case 0:
+            if (sum > 300)
+            {
+                state = 1;
+                attente = 0;
+            }
+            break;
+        case 1:
+            attente++;
+            if (attente > attente_max / 2 && sum < 900)
+            {
+                state = 2;
+                attente = 0;
+                tps_rempli = 0;
+            }
+            if (sum < 50 || sum > 900)
+            {
+                state = 3;
+                attente = 0;
+            }
+            break;
+        case 2:
+            attente++;
+            if ((attente > attente_max * 4) || sum < 50 || tps_rempli > 10)
+            {
+                state = 3;
+                attente = 0;
+                tps_rempli = 0;
+                break;
+            }
+            if (sum > 900)
+            {
+                tps_rempli++;
+            }
+            servo.write(180);
+            break;
+        case 3:
+            servo.write(0);
+            attente++;
+            if (attente > attente_max / 2 && sum < 300)
+            {
+                state = 0;
+                attente = 0;
+            }
+            break;
     }
 
     
